8c4e63b4896785393cf1251c8ab82511b3002fa4
RiLLiaN <srin5ame@gmail.com>
2025-06-06 17:14:01 +0900

    featureCounter

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Counter from "./Counter";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Counter />
       <main className={styles.main}>
         <Image
           className={styles.logo}
@@ -18,6 +20,7 @@ export default function Home() {
             Get started by editing <code>app/page.js</code>.
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>こんにちは</li>
         </ol>
 
         <div className={styles.ctas}>
